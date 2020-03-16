d481d0688fc46c1104efb82ddcdb7d1ea557c9d3
Samuel Adebayo <opeyemishabach@gmail.com>
2020-03-16 15:34:42 +0100

    Web app currently with a sign-up and sign-in page, a shop page and a payment platform... This is the first commit though

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { createStructuredSelector } from 'reselect';
+
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
+
+import { connect } from 'react-redux'
+
+import { selectIsCollectionFetching, selectIsCollectionsLoaded } from '../../redux/shop/shop.selectors'
+
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+
+import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions'
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage)
+
+class ShopPage extends React.Component {
+  componentDidMount() {
+    const {fetchCollectionsStartAsync} = this.props;
+    fetchCollectionsStartAsync();
+  }
+
+
+  render() {
+    const { match, isCollectionFetching } = this.props;
+    
+    return (
+      <div className='shop-page'>
+        <Route 
+        exact path={`${match.path}`} 
+        render = {props => (
+        <CollectionsOverviewWithSpinner isLoading={isCollectionFetching} {...props} />
+        )} />
+        <Route path={`${match.path}/:collectionId`} 
+        render = {props => (
+        <CollectionPageWithSpinner isLoading={isCollectionFetching} {...props} />
+        )} />
+      </div>
+    )}
+} 
+
+const mapStateToProps = createStructuredSelector({
+  isCollectionFetching: selectIsCollectionFetching
+})
+
+const mapDispatchToProps = dispatch => ({
+  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+})
+
+export default connect
+(mapStateToProps,
+mapDispatchToProps)
+(ShopPage);
